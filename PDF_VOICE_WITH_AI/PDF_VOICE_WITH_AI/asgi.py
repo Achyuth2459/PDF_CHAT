7f83d048edda96f34509550cbf9cb46172cd69a5
Achyuth2459 <achyuthchamakuri2459@gmail.com>
2023-10-12 20:14:28 +0530

    changes

---
+++ PDF_VOICE_WITH_AI/PDF_VOICE_WITH_AI/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PDF_VOICE_WITH_AI project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PDF_VOICE_WITH_AI.settings')
+
+application = get_asgi_application()
